5e0d038f2182b8a7685587df27395671368a3c84
bhuwanpandey <bhuwan.pandey9867@gmail.com>
2024-03-31 11:37:02 +0545

    update the chat route and set env variable

--- chats/urls.py
+++ chats/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("auth/",include("users.urls")),
-    path("auth/",include("chatapp.urls")),
+    path("api/",include("chatapp.urls")),
 ]
 
 if settings.DEBUG:
